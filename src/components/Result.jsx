a65e81b811a57f513801a5c19ad6eb4681536918
gabicilfone <cilfonegabriel@gmail.com>
2023-09-25 19:13:52 -0300

    Add style

--- src/components/Result.jsx
+++ src/components/Result.jsx
@@ -1,18 +1,51 @@
 import styled from "@emotion/styled"
 
+const Container = styled.div `
+    color: #FFF;
+    font-family: 'Lato', sans-serif;
+    display: flex;
+    align-items: center;
+    gap: 1rem;
+    margin-top: 30px;
+`
+
+const Imagen = styled.img `
+    display: block;
+    width: 120px;
+`
+
+const Text = styled.p `
+    font-size: 18px;
+    span{
+        font-weight: 700;
+    }
+`
+
+const Price = styled.p `
+    font-size: 24px;
+    span{
+        font-weight: 700;
+    }
+`
+
 const Result = ({result}) => {
 
     const {PRICE, HIGHDAY, LOWDAY, CHANGEPCT24HOUR, IMAGEURL, LASTUPDATE} = result
 
   return (
-    <div>
-      <p>The price is: <span>{PRICE}</span></p>
-      <p>Highest price of the day: <span>{HIGHDAY}</span></p>
-      <p>Lowest price of the day: <span>{LOWDAY}</span></p>
-      <p>Variation last 24 hours: <span>{CHANGEPCT24HOUR}</span></p>
-      <p>Last update: <span>{LASTUPDATE}</span></p>
-
-    </div>
+    <Container>
+      <Imagen 
+        src={`https://cryptocompare.com/${IMAGEURL}`} 
+        alt="Imagen cripto" 
+      />
+      <div>
+        <Price>The price is: <span>{PRICE}</span></Price>
+        <Text>Highest price of the day: <span>{HIGHDAY}</span></Text>
+        <Text>Lowest price of the day: <span>{LOWDAY}</span></Text>
+        <Text>Variation last 24 hours: <span>{CHANGEPCT24HOUR}</span></Text>
+        <Text>Last update: <span>{LASTUPDATE}</span></Text>
+      </div>
+    </Container>
   )
 }
 
